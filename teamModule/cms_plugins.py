51701f9994a47e8e1779ec2980007511b4a7c77e
Alexis Côté <alexiscote19@hotmail.com>
2018-02-22 22:16:18 -0500

    J'ai ajouté le passage des membres au frontend(voir le template)

--- teamModule/cms_plugins.py
+++ teamModule/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 from django.core import serializers
+import json
 from .models import TeamDisplay
 from .models import Team
 from .models import Member
@@ -18,10 +19,11 @@ class TeamModulePlugin(CMSPluginBase):
         teams = list(Team.objects.all().order_by("team_name"))
         teams.insert(0, Team(team_name="All"))
 
-        # members_per_team = list(Member.objects.values())
+        members = Member.objects.select_related()
 
         context = super(TeamModulePlugin, self).render(context, instance, placeholder)
         context.update({
-            'teams': teams
+            'teams': teams,
+            'members': serializers.serialize("json", members)
         })
         return context
