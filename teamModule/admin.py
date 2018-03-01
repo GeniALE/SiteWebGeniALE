aba0430e1b8f4a2137f921aa4b0547b6a4608732
Alexis Côté <alexiscote19@hotmail.com>
2018-02-28 22:21:11 -0500

    Attempt to fix extra info admin panel

--- teamModule/admin.py
+++ teamModule/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+
 from .models import Formation, Team, Project, Member, ProjectStatus, TeamRole, MemberExtraInfo, MemberExtraInfoType
 
 
@@ -11,8 +13,30 @@ class PageProjectsAdmin(admin.ModelAdmin):
     pass
 
 
+# class ExtraInfoInline(admin.StackedInline):
+#    model = Member.extraInfos.through
+#   extra = 1
+
+class ExtraInfoFormSet(BaseInlineFormSet):
+    def get_queryset(self):
+        if not hasattr(self, '_queryset'):
+            criteria = {}  # Your criteria here
+            qs = super(ExtraInfoFormSet, self).get_queryset().filter(**criteria)
+            self._queryset = qs
+        return self._queryset
+
+
+class ExtraInfoInline(admin.StackedInline):
+    model = MemberExtraInfo
+    extra = 0
+    fk_name = 'member'
+    formset = ExtraInfoFormSet
+
+
 class PageMemberAdmin(admin.ModelAdmin):
-    pass
+    model = Member
+    inlines = (ExtraInfoInline,)
+    exclude = ("extraInfos",)
 
 
 class PageFormationAdmin(admin.ModelAdmin):
@@ -26,8 +50,10 @@ class PageStatusAdmin(admin.ModelAdmin):
 class PageTeamRoleAdmin(admin.ModelAdmin):
     pass
 
+
 class PageMemberExtraInfoAdmin(admin.ModelAdmin):
     pass
+
 
 class PageMemberExtraInfoTypeAdmin(admin.ModelAdmin):
     pass
