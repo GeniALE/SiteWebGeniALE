cf823ac57cfd5efaca57a6639f244c095cbd0a5c
Mathieu Chan <Bazooo@users.noreply.github.com>
2018-06-17 21:01:14 -0400

    Feature/beer carousel (#39)
    A slider module to provide information on 🍻 .
    Includes :
    
    Image
    SVG containers (bottle, can...)
    Basic informations on the 🍺
    https://puu.sh/ApXWZ/1601e9f99a.gif
    ---
    * Make the beer carousel html
    
    * Add the beer carousel style and animation
    
    * Add can version
    
    * Update the beer containers
    
    * Update the rotation animation
    
    * Modularized the beer carousel template
    
    * Set beer carousel module with image size
    
    * Add beer count
    
    * Add celsius to the service_temperature
    
    * Revert default.html and put it in website/components
    
    * Clean up landing_page.html
    
    * Add beer_container migration
    
    * Add Next and Back buttons for mobile size

--- beer_carousel/cms_plugins.py
+++ beer_carousel/cms_plugins.py
@@ -1,6 +1,7 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
+import json
 
 from .models import BeerModel, BeerCarouselPluginModel
 
@@ -15,15 +16,20 @@ class BeerCarouselPlugin(CMSPluginBase):
     def get_beers(self):
         return BeerModel.objects.all()
 
+    def get_beer_count(self):
+        return BeerModel.objects.count()
+
     def render(self, context, instance, placeholder):
         if instance and instance.template:
             self.render_template = instance.template
 
         context = super(BeerCarouselPlugin, self).render(context, instance, placeholder)
         beers = self.get_beers()
+        beer_count = self.get_beer_count()
 
         context.update({
             'beers': beers,
+            'beer_count': json.dumps(beer_count),
             'uniqueName': 'beerCarousel' + '__' + str(instance.id),
             'translations': instance.translations
         })
