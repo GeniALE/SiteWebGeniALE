f8c6a325794f40006efd740c137a2d81f7304444
Alexis Côté <popojargo@users.noreply.github.com>
2018-09-12 19:18:24 -0400

    Disable compression when in debug. (#60)
    * Disable compression when in debug.
    
    (We should separate settings per environment in a new pr)
    
    * Add JS cache bust in "DEBUG=True"

---
+++ website/templatetags/cache_bust.py
@@ -0,0 +1,20 @@
+from django import template
+from django.conf import settings
+import os
+import uuid
+
+register = template.Library()
+
+
+# Inspired from: https://stackoverflow.com/questions/27911070/django-wont-refresh-staticfiles
+
+@register.simple_tag(name='cache_bust')
+def cache_bust():
+    if settings.DEBUG:
+        version = uuid.uuid1()
+    else:
+        version = os.environ.get('PROJECT_VERSION')
+        if version is None:
+            version = '1'
+
+    return 'v={version}'.format(version=version)
