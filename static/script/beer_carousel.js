21e67ea6396d6a7ccb1113461192796b1a7922bd
Mathieu Chan <Bazooo@users.noreply.github.com>
2019-04-27 11:38:31 -0400

    Feature/better beer caroussel (#81)
    - Add x and y for scale and shift
    - ParseFloat the values given from the django (fixes the "," given from translating floats to french)
    - Modified the svg for the canisters so they have the same size
    - Added drag
    demo of drag: https://puu.sh/Dk0I6/fd71ad03ec.gif
    
    
    * add scaling on x and y
    
    * fix the sizing of containers
    
    * replace translate literal to basic string concat
    
    * fix animations not working
    
    * remove console.log
    
    * add drag to beer carousel container
    
    * Specify the drag handle on the beer only
    
    * remove console.log and fix template string
    
    * animate beer tag only on switch
    
    * remove useless variable

--- static/script/beer_carousel.js
+++ static/script/beer_carousel.js
@@ -2,6 +2,8 @@ var currentBeerIndex = 0;
 var beerTransitionTime = 1500;
 var beerNumber = 0;
 var timer;
+var dragThreshold = 100;
+var lastRecordedContainerXPosition = 0;
 initBeers();
 
 $('.beercarousel__controls button.beercarousel__button').click(
@@ -14,7 +16,7 @@ $('.beercarousel__prevnextcontrols button.beercarousel__button').click(
     function () {
         var nextBeerIndex = currentBeerIndex == beerNumber - 1 ? 0 : currentBeerIndex + 1;
         if ($(this).hasClass('beercarousel__button--back')) {
-            nextBeerIndex = currentBeerIndex == 0 ? beerNumber - 2 : currentBeerIndex -1;
+            nextBeerIndex = currentBeerIndex == 0 ? beerNumber - 2 : currentBeerIndex - 1;
         }
 
         activateBeer(nextBeerIndex);
@@ -23,23 +25,56 @@ $('.beercarousel__prevnextcontrols button.beercarousel__button').click(
 
 function initBeers() {
     beerNumber = $('div.beer').length;
-    $('.beer__imagetag').each(function (i, el) {
-        var scale = $(el).attr('data-scale');
-        var pos = parseFloat($(el).attr('data-position'));
+    $('.beer__imagetag').each(function (_, el) {
+        var scaleX = parseFloat($(el).attr('data-scale-x'));
+        var scaleY = parseFloat($(el).attr('data-scale-y'));
 
-        $(el).css('transform', tagTransform(scale, pos));
+        var scale = scaleX + ', ' + scaleY;
+
+        var shiftX = parseFloat($(el).attr('data-shift-x'));
+        var shiftY = parseFloat($(el).attr('data-shift-y'));
+
+        $(el).css('transform', tagTransform(scale, shiftX, shiftY));
     });
 
     $('.beer').eq(0).addClass('beer--active');
     $('.beercarousel__controls button.beercarousel__button').eq(0).addClass('beercarousel__button--active');
+
+    // Activate draggable on container
+    var $container = $('.beercarousel__beers');
+
+    $container.draggable({
+        axis: 'x',
+        handle:'.beer__image',
+        stop: function() {
+            checkDrop($container.position().left)
+        }
+    });
 }
 
-function tagTransform(scale, pos) {
-    return 'scale(' + scale + ') translateX(-' + pos + '%)';
+function checkDrop(newPosition) {
+    var difference = Math.abs(lastRecordedContainerXPosition - newPosition);
+    var newIndex = currentBeerIndex;
+
+    // Check if it is over the threshold
+    if (difference > dragThreshold) {
+        // Get the new index
+        newIndex += lastRecordedContainerXPosition > newPosition ? 1 : -1;
+
+        // Clamp index
+        newIndex = Math.min(Math.max(newIndex, 0), beerNumber - 1);
+    }
+
+    activateBeer(newIndex);
+}
+
+function tagTransform(scale, x, y) {
+    return 'scale(' + scale + ') translateX(-' + x + '%) translateY(' + y + '%)';
 }
 
 function activateBeer(index) {
     if (currentBeerIndex === index) {
+        $('.beercarousel__beers').css('left', index * -100 + 'vw');
         return;
     }
 
@@ -59,21 +94,29 @@ function activateBeer(index) {
             return;
         }
 
-        var scale = $(el).attr('data-scale');
-        var shift = parseFloat($(el).attr('data-position'));
+        var scaleX = parseFloat($(el).attr('data-scale-x'));
+        var scaleY = parseFloat($(el).attr('data-scale-y'));
+
+        var scale = scaleX + ', ' + scaleY;
+
+        var shiftX = parseFloat($(el).attr('data-shift-x'));
+        var shiftY = parseFloat($(el).attr('data-shift-y'));
 
         // Animate
         $(el).animate({
-            dest: dest + shift
+            dest: dest + shiftX
         }, {
             duration: beerTransitionTime,
             easing: 'swing',
             step: function (now, tween) {
-                tween.start = origin + shift;
-                $(el).css('transform', tagTransform(scale, now));
+                tween.start = origin + shiftX;
+                $(el).css('transform', tagTransform(scale, now, shiftY));
             },
             complete: function () {
                 timer = 0;
+
+                // update container position
+                lastRecordedContainerXPosition = $('.beercarousel__beers').position().left;
             }
         });
     });
